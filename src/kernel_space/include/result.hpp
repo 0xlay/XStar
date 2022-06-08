420d575eb81b8381e81072281972f7aabab41dde
0xlay <zeroxlay@protonmail.com>
2022-06-08 10:08:35 +0300

    feat(result.hpp): add support Result<const Some&, Error> and Result<Some&, Error>

--- src/kernel_space/include/result.hpp
+++ src/kernel_space/include/result.hpp
@@ -37,7 +37,7 @@ namespace xstar
         ResultError(bool isError, const Error& e) 
             : isError_(isError), error(e)
         {}
-        operator bool() { return isError_; }
+        operator bool() const noexcept { return isError_; }
     private:
         bool isError_;
     public:
@@ -80,7 +80,7 @@ namespace xstar
             }
         }
 
-        Result(Result&& rhs)
+        Result(Result&& rhs) noexcept
         {
             if (&rhs != this)
             {
@@ -100,7 +100,7 @@ namespace xstar
             return *this;
         }
 
-        Result& operator=(Result&& rhs)
+        Result& operator=(Result&& rhs) noexcept
         {
             if (&rhs != this)
             {
@@ -155,6 +155,208 @@ namespace xstar
         union
         {
             Some some_;
+            Error error_;
+        };
+    };
+
+    /*
+    * @brief The Result<const Some&, Error> is the type implements error handling.
+    */
+    template <class Some, class Error>
+    class Result<const Some&, Error> final
+    {
+    public:
+        Result(const Some& some) : isError_(false), some_(&some)
+        { }
+
+        Result(const Error& error) : isError_(true)
+        {
+            new (&error_) Error(error);
+        }
+
+        Result(Error&& error) : isError_(true)
+        {
+            new (&error_) Error(xstar::move(error));
+        }
+
+        Result(const Result& rhs)
+        {
+            if (&rhs != this)
+            {
+                some_ = rhs.some_;
+                new (&error_) Error(rhs.error_);
+            }
+        }
+
+        Result(Result&& rhs) noexcept
+        {
+            if (&rhs != this)
+            {
+                some_ = xstar::move(rhs.some_);
+                new (&error_) Error(xstar::move(rhs.error_));
+            }
+        }
+
+        Result& operator=(const Result& rhs)
+        {
+            if (&rhs != this)
+            {
+                some_ = rhs.some_;
+                new (&error_) Error(rhs.error_);
+            }
+
+            return *this;
+        }
+
+        Result& operator=(Result&& rhs) noexcept
+        {
+            if (&rhs != this)
+            {
+                some_ = xstar::move(rhs.some_);
+                new (&error_) Error(xstar::move(rhs.error_));
+            }
+
+            return *this;
+        }
+
+        ~Result() noexcept
+        {
+            if (isError_)
+            {
+                error_.~Error();
+            }
+        }
+
+        /*
+        * @brief Returns error as ResultError<Error> type
+        * @return ResultError<Error> type, which is a proxy object, that represents an error, if there is one.
+        */
+        ResultError<Error> isError()
+        {
+            return ResultError(isError_, error_);
+        }
+
+        /*
+        * @brief If success then unwrapping Some, otherwise throw KeBugCheckEx().
+        * @param[in] code(optional) - bug check code, which pass in the KeBugCheckEx function.
+        * @return "Some" type, which moved from the Result<Some, Error>.
+        */
+        const Some& unwrap([[maybe_unused]] ULONG code = NO_EXCEPTION_HANDLING_SUPPORT)
+        {
+            if (isError_)
+            {
+                NT_ASSERTMSGW(L"You didn't handle xstar::Result<Error> !", FALSE);
+                ::KeBugCheckEx(code, 0, 0, 0, 0);
+            }
+            return *some_;
+        }
+
+    private:
+        bool isError_;
+        union
+        {
+            const Some* some_;
+            Error error_;
+        };
+    };
+
+    /*
+    * @brief The Result<Some&, Error> is the type implements error handling.
+    */
+    template <class Some, class Error>
+    class Result<Some&, Error> final
+    {
+    public:
+        Result(Some& some) : isError_(false), some_(&some)
+        { }
+
+        Result(const Error& error) : isError_(true)
+        {
+            new (&error_) Error(error);
+        }
+
+        Result(Error&& error) : isError_(true)
+        {
+            new (&error_) Error(xstar::move(error));
+        }
+
+        Result(const Result& rhs)
+        {
+            if (&rhs != this)
+            {
+                some_ = rhs.some_;
+                new (&error_) Error(rhs.error_);
+            }
+        }
+
+        Result(Result&& rhs) noexcept
+        {
+            if (&rhs != this)
+            {
+                some_ = xstar::move(rhs.some_);
+                new (&error_) Error(xstar::move(rhs.error_));
+            }
+        }
+
+        Result& operator=(const Result& rhs)
+        {
+            if (&rhs != this)
+            {
+                some_ = rhs.some_;
+                new (&error_) Error(rhs.error_);
+            }
+
+            return *this;
+        }
+
+        Result& operator=(Result&& rhs) noexcept
+        {
+            if (&rhs != this)
+            {
+                some_ = xstar::move(rhs.some_);
+                new (&error_) Error(xstar::move(rhs.error_));
+            }
+
+            return *this;
+        }
+
+        ~Result() noexcept
+        {
+            if (isError_)
+            {
+                error_.~Error();
+            }
+        }
+
+        /*
+        * @brief Returns error as ResultError<Error> type
+        * @return ResultError<Error> type, which is a proxy object, that represents an error, if there is one.
+        */
+        ResultError<Error> isError()
+        {
+            return ResultError(isError_, error_);
+        }
+
+        /*
+        * @brief If success then unwrapping Some, otherwise throw KeBugCheckEx().
+        * @param[in] code(optional) - bug check code, which pass in the KeBugCheckEx function.
+        * @return "Some" type, which moved from the Result<Some, Error>.
+        */
+        Some& unwrap([[maybe_unused]] ULONG code = NO_EXCEPTION_HANDLING_SUPPORT)
+        {
+            if (isError_)
+            {
+                NT_ASSERTMSGW(L"You didn't handle xstar::Result<Error> !", FALSE);
+                ::KeBugCheckEx(code, 0, 0, 0, 0);
+            }
+            return *some_;
+        }
+
+    private:
+        bool isError_;
+        union
+        {
+            Some* some_;
             Error error_;
         };
     };
